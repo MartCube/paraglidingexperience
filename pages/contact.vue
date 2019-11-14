d4c46eeb72700b8954f20391d8e8a609381dbd68
mart cube <martingjorceski@gmail.com>
2019-11-14 20:24:35 +0100

    form

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,9 +1,10 @@
 <template>
 	<div class="container">
 		<div class="contact">
-			<ValidationObserver ref="observer">
-				<!-- <form ref="contact" class="form" name="contact" action="/submit" data-netlify="true" method="POST" autocomplete="off" @submit.prevent="submit()"> -->
-				<form ref="contact" class="form" name="contact" action="/submit" @submit.prevent="submit()">
+			<form ref="contact" class="form" name="contact" action="/submit" data-netlify="true" method="POST" autocomplete="off" @submit.prevent="submit()">
+			<!-- <form ref="contact" class="form" name="contact" action="/submit" @submit.prevent="submit()"> -->
+				<ValidationObserver ref="observer">
+
 					<h1>What adventures you are planning?</h1>
 					<!-- <h3>Call us: <i class="icon icon-whatsapp"></i> +(27) 797 724 652</h3> -->
 					<h3>Write us a message:</h3>
@@ -34,8 +35,8 @@
 					<button class="btn" type="submit">
 						Send
 					</button>
-				</form>
-			</ValidationObserver>
+				</ValidationObserver>
+			</form>
 		</div>
 
 		<!-- <div class="google_map">
